e98dc13cbbccece45c0b9b4f39f4a1e9b70afe64
katopodis@mail.ru <katopodis@mail.ru>
2020-05-07 13:59:32 +0400

    add header footer, leyaout, register template,register code

--- resources/views/auth/confirmCode.blade.php
+++ resources/views/auth/confirmCode.blade.php
@@ -2,21 +2,28 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <section class="step2_container">
+        <div id="steps_block">
+            <a href=""><span class="span1"><i class="fas fa-check-circle"></i></span>{{__('global.basicInfo')}}</a>
+            <a href="" class="active_a"><span class="span2">2</span>{{__('global.confirmation')}}</a>
+            <a href=""><span class="span3">3</span>{{__('global.completing')}}</a>
+        </div>
+        <div id="confirmation_container">
+            <div id="confirmation">
+                <h3>{{__('global.confirmation')}}</h3>
+                <p>{{__('global.YouNeedConfEmail')}}</p>
+            </div>
+            <div id="code_from_email_container">
+                <p>{{__('global.textConfirmationBladePart1')}} <span>{{$user->email}}</span>.{{__('global.textConfirmationBladePart2')}} </p>
+                <p class="Code_email_p">{{__('global.codeFromEmail')}}</p>
+                <form class="d-inline Code_email_p" method="POST" action="{{ route('confirm', $user) }}">
+                    @csrf
+                    <input type="text" name="token">
+                    <button type="submit" class="confirm_email">{{ __('Confirm email') }}</button>.
+                </form>
 
-                    <div class="card-body">
-                        <form class="d-inline" method="POST" action="{{ route('confirm', $user) }}">
-                            @csrf
-                            <input type="text" name="token">
-                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                        </form>
-                    </div>
-                </div>
+
             </div>
         </div>
-    </div>
+    </section>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,87 +1,8 @@
-{{--<div id="app">--}}
-{{--    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">--}}
-{{--        <div class="container">--}}
-{{--            <header>--}}
-{{--                <div class="heading">--}}
-{{--                    <div class="menu">--}}
-{{--                        @if(auth()->user())--}}
-{{--                            <div class="iconLines" ><div><img src="{{asset("images/iconMenu.jpg")}}" onClick="display()"></div> </div>--}}
-{{--                        @endif--}}
-{{--                        <a class="navbar-brand" href="{{ url('/') }}"><div class="img"><img src={{asset("images/logo3.png")}} > </div></a>--}}
-{{--                        <div class="search_field">--}}
-{{--                            <input type="text" placeholder="{{__("global.search_cashBack")}}">--}}
-{{--                            <div type="submit">--}}
-{{--                                <i class="fa fa-search"></i>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="buttons">--}}
-{{--                            <div class="user"> <img src="images//user.png"></div>--}}
-{{--                        </div>--}}
-{{--                    <!-- navbar-laravel-->--}}
-{{--                        <nav class="navbar navbar-expand-md navbar-light">--}}
-{{--                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-{{--                                <span class="navbar-toggler-icon"></span>--}}
-{{--                            </button>--}}
-
-{{--                            <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-{{--                                <!-- Left Side Of Navbar -->--}}
-{{--                                <ul class="navbar-nav mr-auto">--}}
-
-{{--                                </ul>--}}
-
-{{--                                <!-- Right Side Of Navbar -->--}}
-{{--                                <ul class="navbar-nav ml-auto">--}}
-{{--                                    <!-- Authentication Links -->--}}
-{{--                                    @guest--}}
-{{--                                        <li class="nav-item">--}}
-{{--                                            <a id="login" class="nav-link" href="{{ route('login') }}">{{ __('form.login') }}</a>--}}
-{{--                                        </li>--}}
-{{--                                        @if (Route::has('register'))--}}
-{{--                                            <li class="nav-item">--}}
-{{--                                                <a id="signup"  class="nav-link" href="{{ route('register') }}">{{ __('form.register') }}</a>--}}
-{{--                                            </li>--}}
-{{--                                        @endif--}}
-{{--                                    @else--}}
-{{--                                        <li class="nav-item dropdown">--}}
-{{--                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
-{{--                                                {{ Auth::user()->name }} <span class="caret"></span>--}}
-{{--                                            </a>--}}
-
-{{--                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
-{{--                                                <a class="dropdown-item" href="{{route("user.account")}}">{{__("form.account")}}</a>--}}
-{{--                                                <a class="dropdown-item" href="{{route("user.changePass")}}">{{__("form.changePass")}}</a>--}}
-{{--                                                <a class="dropdown-item" href="{{ route('logout') }}"--}}
-{{--                                                   onclick="event.preventDefault();--}}
-{{--                                                                     document.getElementById('logout-form').submit();">--}}
-{{--                                                    {{ __('global.logout') }}--}}
-{{--                                                </a>--}}
-
-
-{{--                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-{{--                                                    @csrf--}}
-{{--                                                </form>--}}
-{{--                                            </div>--}}
-{{--                                        </li>--}}
-{{--                                    @endguest--}}
-{{--                                </ul>--}}
-{{--                            </div>--}}
-{{--                        </nav>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </header>--}}
-{{--        </div>--}}
-
-
-{{--    <main class="py-4">--}}
-{{--        @yield('content')--}}
-{{--    </main>--}}
-{{--</div>--}}
-
 <!-- start header -->
 <div id="app">
     <header>
         <nav class="navbar navbar-expand-lg navbar-light ">
-            <a class="navbar-brand logo" href="#"><img src="img/logo1.png" alt=""></a>
+            <a class="navbar-brand logo" href="{{route('home')}}"><img src="{{asset('img/logo1.png')}}" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -90,22 +11,22 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Find Job<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">{{__('global.findJob')}}<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Find Translator<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">{{__('global.findTranslator')}}<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">How It Works<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">{{__('global.howItWorks')}}<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">{{__('global.about')}}<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Blog<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">{{__('global.blog')}}<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Help<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">{{__('global.help')}}<span class="sr-only">(current)</span></a>
                     </li>
 
                 </ul>
@@ -128,12 +49,12 @@
                             <!-- Authentication Links -->
                             @guest
                                 <li class="nav-item">
-                                    <a id="login" class="nav-link btn  my-2 my-sm-0 login"
+                                    <a {{--id="login"--}} class="<!--nav-link--> btn  my-2 my-sm-0 login"
                                        href="{{ route('login') }}">{{ __('form.login') }}</a>
                                 </li>
                                 @if (Route::has('register'))
                                     <li class="nav-item">
-                                        <a id="signup" class="nav-link btn  my-2 my-sm-0 sign"
+                                        <a {{--id="signup"--}} class="<!--nav-link--> btn  my-2 my-sm-0 sign"
                                            href="{{ route('register') }}">{{ __('form.register') }}</a>
                                     </li>
                                 @endif
@@ -141,14 +62,14 @@
                                 <li class="nav-item dropdown">
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                        {{ Auth::user()->first_name }} <span class="caret"></span>
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item"
-                                           href="{{route("user.account")}}">{{__("form.account")}}</a>
-                                        <a class="dropdown-item"
-                                           href="{{route("user.changePass")}}">{{__("form.changePass")}}</a>
+{{--                                        <a class="dropdown-item"--}}
+{{--                                           href="{{route("user.account")}}">{{__("form.account")}}</a>--}}
+{{--                                        <a class="dropdown-item"--}}
+{{--                                           href="{{route("user.changePass")}}">{{__("form.changePass")}}</a>--}}
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                                      document.getElementById('logout-form').submit();">
@@ -166,10 +87,6 @@
                         </ul>
                     </div>
                 </nav>
-{{--                <form class="form-inline my-2 my-lg-0">--}}
-{{--                    <button class="btn  my-2 my-sm-0 login">Log In</button>--}}
-{{--                    <button class="btn my-2 my-sm-0 sign">Sign UP</button>--}}
-{{--                </form>--}}
             </div>
         </nav>
     </header>
